d6d22b1444e9ea035d15e436ab813fdf9342bf47
Raffaele Salmaso <raffaele@salmaso.org>
2009-10-06 19:45:40 +0200

    news application

---
+++ news/admin.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2007-2009, Salmaso Raffaele <raffaele@salmaso.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+from fluo import admin
+from news.models import News, Category, NewsTranslation
+
+MAX_LANGUAGES = len(settings.LANGUAGES)
+
+class CategoryAdminForm(admin.ModelForm):
+    class Meta:
+        model = Category
+class CategoryAdmin(admin.CategoryModelAdmin):
+    model = Category
+    form = CategoryAdminForm
+admin.site.register(Category, CategoryAdmin)
+
+class NewsTranslationInlineModelForm(admin.ModelForm):
+    class Meta:
+        model = NewsTranslation
+class NewsTranslationInline(admin.TabularInline):
+    model = NewsTranslation
+    form = NewsTranslationInlineModelForm
+    extra = MAX_LANGUAGES
+    max_num = MAX_LANGUAGES
+    fields = ('language', 'title', 'abstract', 'text',)
+class NewsAdminForm(admin.ModelForm):
+    class Meta:
+        model =  News
+class NewsAdmin(admin.OrderedModelAdmin):
+    model = News
+    form = NewsAdminForm
+    list_display = ('__unicode__', 'status', 'event_date', 'pub_date_begin', 'pub_date_end', '_get_categories', '_get_users',)
+    list_display_links = ('__unicode__',)
+    list_per_page = 30
+    ordering = ("ordering",)
+    fieldsets = (
+        (None, {"fields": (
+            ('status', 'ordering',),
+            'title',
+            'event_date',
+            ('pub_date_begin', 'pub_date_end',),
+            'abstract',
+            'text',
+            'note',
+        ),}),
+        (_('Show to'), {'fields': ('users',),}),
+    )
+    filter_horizontal = ('users', 'categories',)
+    inlines = (NewsTranslationInline,)
+
+    def _get_users(self):
+        users = self.users.all().order_by('username')
+        if users:
+            return u', '.join([user.username for user in users])
+        else:
+            return _(u'All')
+    _get_users.short_description = _('Show to')
+
+    def _get_categories(self):
+        categories = self.categories.all().order_by('name')
+        if categories:
+            return u', '.join([category.name for category in categories])
+        else:
+            return _(u'All')
+    _get_categories.short_description = _('Categories')
+
+admin.site.register(News, NewsAdmin)
+
---
+++ news/models.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2007-2009, Salmaso Raffaele <raffaele@salmaso.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+from fluo import models
+
+class Category(models.CategoryModel):
+    class Meta:
+        verbose_name = _('News type')
+        verbose_name_plural = _('News types')
+
+DRAFT = 'draft'
+PUBLISHED = 'published'
+
+STATUS_CHOICES = (
+    (DRAFT, _('Draft')),
+    (PUBLISHED, _('Published')),
+)
+
+class NewsManager(models.Manager):
+    def draft(self):
+        return self.filter(status=DRAFT)
+    def published(self):
+        return self.filter(status=PUBLISHED)
+
+class News(models.OrderedModel, models.I18NModel):
+    objects = NewsManager()
+
+    status = models.StatusField(
+        choices=STATUS_CHOICES,
+        help_text=_('If should be displayed or not.'),
+    )
+    categories = models.ManyToManyField(
+        Category,
+        null=True,
+        blank=True,
+        related_name="news",
+        verbose_name=_('Categories')
+    )
+    users = models.ManyToManyField(
+        User,
+        blank=True,
+        null=True,
+        related_name='news',
+        verbose_name=_('Visible only to'),
+        help_text=_('News visible to these users, if empty is visible to all users.'),
+    )
+    event_date = models.DateTimeField(
+        blank=True,
+        null=True,
+        verbose_name=_('News date'),
+        help_text=_('Date which news refers to.'),
+    )
+    pub_date_begin = models.DateTimeField(
+        blank=True,
+        null=True,
+        verbose_name=_('Publication date begin'),
+        help_text=_('When news publication date begins.'),
+    )
+    pub_date_end = models.DateTimeField(
+        blank=True,
+        null=True,
+        verbose_name=_('Publication date end'),
+        help_text=_('When news publication date ends.'),
+    )
+    title = models.CharField(
+        unique=True,
+        max_length=255,
+        verbose_name=_('Title'),
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=255,
+        verbose_name=_('Slug field'),
+    )
+    abstract = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        verbose_name=_('Abstract'),
+        help_text=_('A brief description of the news'),
+    )
+    text = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name=_('Default text'),
+    )
+    note = models.TextField(
+        blank=True,
+        verbose_name=_('Note'),
+    )
+
+    class Meta:
+        verbose_name = _("News")
+        verbose_name_plural = _("News")
+        unique_together = (('title', 'slug',),)
+
+    def __unicode__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(News, self).save(*args, **kwargs)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('news-detail', (), {'slug': self.translate().slug})
+
+class NewsTranslation(models.TranslationModel):
+    news = models.ForeignKey(
+        News,
+        related_name='translations',
+        verbose_name=_('News type'),
+    )
+    title = models.CharField(
+        unique=True,
+        max_length=255,
+        verbose_name=_('Title'),
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=255,
+        verbose_name=_('Slug field'),
+    )
+    abstract = models.CharField(
+        max_length=255,
+        verbose_name=_('Abstract'),
+        help_text=_('A brief description'),
+    )
+    text = models.TextField(
+        verbose_name=_('Body'),
+    )
+
+    class Meta:
+        verbose_name = _("News translation")
+        verbose_name_plural = _("News translations")
+        unique_together = (('news', 'language',), ('title', 'slug',))
+
+    def __unicode__(self):
+        return u'%s (%s)' % (self.news, self.language)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(NewsTranslation, self).save(*args, **kwargs)
+
