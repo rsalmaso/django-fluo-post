97df14497a9fdf9b29ff5b5cb1b1cd96be190ede
Raffaele Salmaso <raffaele@salmaso.org>
2010-02-04 17:38:11 +0100

    removed unused import

--- news/urls.py
+++ news/urls.py
@@ -21,7 +21,6 @@
 # THE SOFTWARE.
 
 from django.conf.urls.defaults import *
-from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^$', 'news.views.news_list', name='news-list'),
