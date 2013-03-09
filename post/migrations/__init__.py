f53db53d038ec41a599d137167d844b170f8fd18
Raffaele Salmaso <raffaele@salmaso.org>
2013-03-09 23:52:17 +0100

    update copyright year

--- post/migrations/__init__.py
+++ post/migrations/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2007-2012, Raffaele Salmaso <raffaele@salmaso.org>
+# Copyright (C) 2007-2013, Raffaele Salmaso <raffaele@salmaso.org>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -19,3 +19,4 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+
